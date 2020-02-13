364bba0337f8d8620e7bc6bdaddc8378505fbe4b
David Golodetsky <davyd.lito@gmail.com>
2020-02-13 22:13:28 +0200

    wd

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,53 +1,12 @@
 <template>
-  <v-footer color="primary lighten-1" padless>
-    <v-layout justify-center wrap>
-      <v-btn
-        v-for="(item, index) in navItems"
-        :key="index"
-        color="white"
-        :to="item.link"
-        text
-        rounded
-        class="my-2"
-      >{{ item.text }}</v-btn>
-      <v-flex primary lighten-2 py-4 text-center white--text xs12>
-        &copy; {{ new Date().getFullYear() }} —
-        <strong>Geek Score</strong>
-      </v-flex>
+  <v-footer padless>
+    <v-layout justify-center wrap primary py-3 text-center white--text xs12>
+      &copy; {{ new Date().getFullYear() }} —
+      <strong>Geek Score</strong>
     </v-layout>
   </v-footer>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-
-export default {
-  computed: {
-    ...mapGetters("user", ["user"]),
-    navItems() {
-      if (this.user) {
-        return [
-          {
-            text: "Games",
-            icon: "cards",
-            link: "/games"
-          }
-        ];
-      } else {
-        return [
-          {
-            text: "Sign up",
-            icon: "account-circle ",
-            link: "/signup"
-          },
-          {
-            text: "Sign in",
-            icon: "login",
-            link: "/signin"
-          }
-        ];
-      }
-    }
-  }
-};
+export default {};
 </script>
