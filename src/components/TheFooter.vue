61129362e562714449027db22e7ca4892d264c4a
David Golodetsky <davyd.lito@gmail.com>
2020-05-18 20:11:52 +0300

    by

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,7 +3,10 @@
     <v-layout justify-space-between py-2 wrap primary text-center white--text xs12>
       <v-container class="d-flex flex-wrap justify-space-between footer-text">
         <span class="mr-6">&copy; {{ new Date().getFullYear() }} — Geek Score</span>
-        <span>geekscore.info@gmail.com</span>
+        <span>
+          Build by
+          <a href="https://twitter.com/david_go__">@David_Go__</a>
+        </span>
       </v-container>
     </v-layout>
   </v-footer>
