3b9e9c4df86ca6c4128a2e5a2c267a5c899e35d6
David Golodetsky <lc@lcs-MacBook-Pro.local>
2021-04-06 18:06:45 +0300

    tw

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,11 +1,25 @@
 <template>
-  <v-footer class="the-footer" padless>
-    <v-layout justify-space-between py-2 wrap primary text-center white--text xs12>
+  <v-footer
+    class="the-footer"
+    padless
+  >
+    <v-layout
+      justify-space-between
+      py-2
+      wrap
+      primary
+      text-center
+      white--text
+      xs12
+    >
       <v-container class="d-flex flex-wrap justify-space-between footer-text">
         <span class="mr-6">&copy; {{ new Date().getFullYear() }} — Geek Score</span>
         <span>
           Build by
-          <a class="link" href="https://twitter.com/david_go__">@David_Go__</a>
+          <a
+            class="link"
+            href="https://twitter.com/dvdgld"
+          >@dvdgld</a>
         </span>
       </v-container>
     </v-layout>
